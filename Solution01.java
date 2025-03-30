83dc447eef29b7557b063d6682e76c70df9aa9d1
SSSSJIN <shoes90@naver.com>
2025-03-30 10:18:14 +0900

    250330_Algorithm
    프로그래머스 입문 문제입니다.

---
+++ Solution01.java
@@ -0,0 +1,11 @@
+package kr.ac.kopo.programmers;
+
+class Solution01 {
+    public int solution(int num1, int num2) {
+        if(num1 == num2){
+        return 1;         
+        } else {            
+        }
+        return -1;
+    }
+}
